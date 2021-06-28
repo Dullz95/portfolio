6538c03ce8d78e0db4bd69c089c9cdf9079e21ae
Dullz95 <82204773+Dullz95@users.noreply.github.com>
2021-06-28 11:37:15 +0200

    update

---
+++ Desktop/portfolio-main/scripts.js
@@ -0,0 +1,66 @@
+// navbar
+function toggleNavbar() {
+  document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
+}
+
+
+// full page
+// new fullpage("#fullpage", {
+//   anchors: ["home", "about", "resume", "projects", "testimonial", "contact"],
+//   navigation: true,
+//   navigationPosition: "right",
+//   navigationTooltips: [
+//     "home",
+//     "about",
+//     "resume",
+//     "projects",
+//     "testimonial",
+//     "contact",
+//   ],
+//   center: true,
+// });
+// owl
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    loop: true,
+    dots: false,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+        // nav:true
+      },
+      1000: {
+        items: 3,
+        // nav:false
+      },
+      // 1000:{
+      // items:5,
+      // nav:true,
+      // loop:false
+      // }
+    },
+  });
+});
+
+function switchTab(tabID) {
+  let tabs = document.getElementsByClassName("resume-container");
+
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("active");
+  }
+
+  document.getElementById(tabID).classList.add("active");
+}
+
+let navigation = document.getElementsByClassName("navbar-links")[0];
+let items = document.getElementsByClassName("navbar-link");
+
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener('click', close);
+}
+
+function close(e){
+    navigation.classList.remove("active")
+};
